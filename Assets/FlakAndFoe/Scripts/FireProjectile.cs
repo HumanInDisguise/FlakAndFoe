b62e69c9c57291d56988b3b53485f6b43a919a2a
JustinC <justin.clark298@gmail.com>
2020-05-12 20:57:52 +1200

    0.5 Week 8
    Applying week 8 stuff

---
+++ Assets/FlakAndFoe/Scripts/FireProjectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Projectiles
+
+{
+    public class FireProjectile : MonoBehaviour
+    {
+        // The header is simply a label that will appear in the Inspector.
+        [Header("Public references")]
+        public Transform bulletOrigin;
+        public GameObject bullet;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // If fire1 input is received
+            if (Input.GetButton("Fire1"))
+            {
+                // We use a method to fire the bullet
+                FireBullet();
+            }
+        }
+
+        void FireBullet()
+        {
+            //Testing that the fire method is being called
+            // Debug.Log("FIring bullet");
+
+            // Instantiate the bullet at the point of origin
+            Instantiate(bullet, bulletOrigin.position, bulletOrigin.rotation);
+
+        }
+
+
+
+    }
+}
---
+++ Assets/FlakAndFoe/Scripts/PlayerBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThePlayerBullet
+{
+
+    public class PlayerBullet : MonoBehaviour
+    {
+
+        [Header("Bullet Settings")]
+        public float speed = 30;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //	Gives the bullet forward force when instantiated
+            gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed, ForceMode.Impulse);
+        }
+
+    }
+}
